68c07aa97b7cd4b0ea0def436318152457a1a2db
徐培堯 <st89382000@gmail.com>
2017-06-12 15:00:23 +0800

    修正位置異常問題

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,15 @@
 	var passwordList = document.querySelectorAll("input[type='password']");
 	for(let i = 0 ; i < passwordList.length ; i++){
 		if(passwordList[i].attributes['data-showPassword'] == true)continue;
-		var showButton = document.createElement("div");
+		let showButton = document.createElement("div");
 		showButton.classList.add("showPwdButton");
 		
-		showButton.style.height = passwordList[0].clientHeight + "px";
+		showButton.style.height = (passwordList[0].clientHeight 
+			+ parseInt(getComputedStyle(passwordList[i]).borderBottom)
+			+ parseInt(getComputedStyle(passwordList[i]).borderTop))
+			+ "px";
 		showButton.style.width = "18px";
-
+		showButton.style.display = "inline";
 		var mouseDown = function(){
 			showButton.classList.add("active");
 			passwordList[i].type="text";
@@ -20,13 +23,17 @@
 		showButton.onmouseup = mouseUp;
 		showButton.onmouseleave = mouseUp;
 		
-		passwordList[i].style.width = "calc(100% - " + showButton.style.width + ")"
+		passwordList[i].style.setProperty("width", "calc(" + getComputedStyle(passwordList[i]).width + " - " + showButton.style.width + ")", "important");
+		passwordList[i].style.display = "inline";
 		
+		insertAfter(showButton,passwordList[i])
 		
-		passwordList[i].parentNode.insertBefore(showButton,passwordList[i]);
-		
-		
+		//passwordList[i].parentNode.insertBefore(showButton,passwordList[i]);
+				
 		passwordList[i].attributes['data-showPassword'] = true;
 	}
+}
+function insertAfter(newNode, referenceNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
 setInterval(main,1000);
